32cfafbb24d01b1138611ddd7abc087d4a8cc4b0
taylorbohl <tbohl3@gmail.com>
2022-08-14 16:13:21 -0500

    cleaned up app.py with comments, added tableau visuals, adjusted image sizes for form, removed duplicate form html file

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
-# import necessary libraries
+# Import necessary libraries
 from pickle import load
 import numpy as np
 from flask import Flask, render_template, request
 
-#################################################
 # Flask Setup
-#################################################
 app = Flask(__name__)
 
-
-#################################################
 # Load ML Model
-#################################################
 model = load(open('model_randomforrest_2022080848.pkl', 'rb'))
-# load the scaler
+# Load the Scaler
 scaler = load(open('scaler.pkl', 'rb'))
 
-# create route that renders index.html template
+# Create routes for each html template
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -29,33 +24,23 @@ def visual():
 def models():
     return render_template("models.html")
 
-# create route that renders index.html template
+# Create routes for form entry
 @app.route("/form")
 def form():
-
-    # # Method 1 inputs
-    # pclass = ""
-    # name = ""
-    # sex = ""
-    # age = ""
-    # family = ""
-    # ticket = ""
-    # fare = ""
-    # embarked = ""
 
     prediction_text = ""
 
     return render_template("predictor.html", result = prediction_text)
 
-# Query the database and send the jsonified results
 @app.route("/send", methods=["POST"])
 def send():
+    # Define variables for one hot coded variables
     firstclass= 0
     secondclass= 0
     thirdclass= 0
     male= 0
     female= 0
-    # # Method 1:  Obtain form inputs and add to numpy array or dataframe
+    # Obtain form inputs and add to numpy array or dataframe
     name = request.form["survivedname"]
     age = float(request.form["survivedage"])
     fare = float(request.form['survivedfare'])
@@ -73,30 +58,24 @@ def send():
         male= 1
     else:
         female= 1
-
+    # List with feature entries to send to model for prediction
     features= [age, converted_fare, family, firstclass, secondclass, thirdclass, female, male]
-
-    
-    # transform the test dataset
+   
+    # Transform list into numpy array and apply scaler
     final_features = [np.array(features)]
     features_scaled = scaler.transform(final_features)
-    
 
-    # Method 2:  Obtain form inputs and add to numpy array   
-    # features = [float(x) for x in request.form.values()]
-    # final_features = [np.array(features)]
-
-    # # use form results to make prediction
+    # Use form results to make prediction
     prediction = model.predict(features_scaled)[0]
 
-    # create html content - either single variable, dictionary, or string
+    # Create html content with prediction result
     if prediction == 1: 
         prediction_text = f"Congratulations, {(name)}! You survived!"
     else:
         prediction_text = f"Sorry {(name)}- You did not survive."
     display_form_input = f"You played as a {(int(age))} year old {(sex)}, traveling with {int((family))} family members. You paid ${(int(fare))} to travel in {(pclass)} class."
 
-    # send prediction to html page
+    # Send prediction to html page
     return render_template("predictor.html", form_input = display_form_input, result = prediction_text)
 
 
